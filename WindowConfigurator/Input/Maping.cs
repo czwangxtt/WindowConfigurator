75d61a054433c7efd4af384b1fd56fc7fdbb635c
CWangSchuco <CWang@schuco-usa.com>
2020-03-12 10:37:50 -0400

    Create a new Mapping class, remove function to read and convert json file into it

--- WindowConfigurator/Input/Maping.cs
+++ WindowConfigurator/Input/Maping.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace WindowConfigurator.Input
 {
     class Maping
     {
+        private Dictionary<string, object> storage = new Dictionary<string, object>();
+        private JsonInput jsonInput = new JsonInput();
+
+        public Maping()
+        {
+            string fileName = @"d:\a.json";
+            string input = File.ReadAllText(fileName);
+            jsonInput = JsonConvert.DeserializeObject<JsonInput>(input);
+        }
         
     }
 }
--- WindowConfigurator/Program.cs
+++ WindowConfigurator/Program.cs
@@ -18,7 +18,6 @@ namespace WindowConfigurator
     class Program
     {
         
-
         static void Main(string[] args)
         {
             Stopwatch stopWatch = new Stopwatch();
@@ -27,85 +26,37 @@ namespace WindowConfigurator
             string fileName = @"d:\a.json";
             string input = File.ReadAllText(fileName);
             //Console.WriteLine(input);
-            JsonInput deserializedInput = JsonConvert.DeserializeObject<JsonInput>(input);
+            WindowInput deserializedInput = JsonConvert.DeserializeObject<WindowInput>(input);
 
-            //Window window = new Window(deserializedInput);
+            Window window = new Window(deserializedInput);
 
-            //Transom t1 = new Transom(new Point3(0, 0, 1000), new Point3(0, 1500, 1000));
-            //Transom t2 = new Transom(new Point3(0, 0, 2000), new Point3(0, 1500, 2000));
-            //Mullion m1 = new Mullion(new Point3(0, 750, 1000), new Point3(0, 750, 2000));
-            //window.wireFrame.addIntermediate(t1);
-            //window.wireFrame.addIntermediate(t2);
-            //window.wireFrame.addIntermediate(m1);
-            //window.wireFrame.removeIntermediate(t1);
-            //window.wireFrame.removeIntermediate(t2);
+            Transom t1 = new Transom(new Point3(0, 0, 1000), new Point3(0, 1500, 1000));
+            Transom t2 = new Transom(new Point3(0, 0, 2000), new Point3(0, 1500, 2000));
+            Mullion m1 = new Mullion(new Point3(0, 750, 1000), new Point3(0, 750, 2000));
+            window.wireFrame.addIntermediate(t1);
+            window.wireFrame.addIntermediate(t2);
+            window.wireFrame.addIntermediate(m1);
+            window.wireFrame.removeIntermediate(t1);
+            window.wireFrame.removeIntermediate(t2);
 
-            //JsonSerializer serializer = new JsonSerializer();
-            //serializer.NullValueHandling = NullValueHandling.Ignore;
-            //serializer.NullValueHandling = NullValueHandling.Ignore;
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+            serializer.NullValueHandling = NullValueHandling.Ignore;
 
-            //using (StreamWriter sw = new StreamWriter(@"d:\c.json"))
-            //using (JsonWriter writer = new JsonTextWriter(sw))
-            //{
-            //    serializer.Serialize(writer, deserializedInput);
-            //}
+            using (StreamWriter sw = new StreamWriter(@"d:\c.json"))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, deserializedInput);
+            }
 
-            //string output = JsonConvert.SerializeObject(deserializedInput);
-            //Console.WriteLine(output);
+            string output = JsonConvert.SerializeObject(deserializedInput);
+            Console.WriteLine(output);
 
-            //stopWatch.Stop();
-            //Console.WriteLine(stopWatch.ElapsedMilliseconds.ToString());
-
-
-
-            //SortedList<double, int> sl = new SortedList<double, int>();
-            //sl.Add(0.0, 1);
-            //sl.Add(10.0, 2);
-            //sl.Add(3.0, 3);
-            //sl.Add(4.0, 4);
-            //sl.Add(2.0, 5);
-
-            //for (int i = 0; i < sl.Count; i++)
-            //{
-            //    Console.WriteLine("\t{0}:\t{1}", sl.Keys[i], sl[sl.Keys[i]]);
-            //}
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.ElapsedMilliseconds.ToString());
 
 
 
-            //void graph(Point center, int length)
-            //{
-            //    for (int i = 0; i < center.y; i++)
-            //        Console.WriteLine(" ");
-            //    for (int i = 0; i < length; i++)
-            //    {
-            //        string line = " ";
-            //        for (int j = 0; j < center.x * 2 + length * 2 - 1; j++)
-            //        {
-            //            if (i == 0 || i == length - 1)
-            //            {
-            //                if (j >= center.x * 2 && (j - center.x * 2) % 2 == 0)
-            //                    line += "*";
-            //                else line += " ";
-            //            }
-            //            else
-            //            {
-            //                if (j == center.x * 2 || j == center.x * 2 + length * 2 - 2)
-            //                    line += "*";
-            //                else line += " ";
-            //            }
-            //        }
-            //        Console.WriteLine(line);
-            //    }
-            //}
-
-            //WireFrame wireFrame = new WireFrame(10.0, 10.0);
-
-
-
-
-
-            //Product deserializedProduct = JsonConvert.DeserializeObject<Product>(output);
-            //Console.WriteLine(deserializedProduct.Price);
         }
     }
 }
